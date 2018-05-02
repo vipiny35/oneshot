41ae6e56e70da4b18083b2bb9a748772e16ba133
Rishabh Kandari <reevkandari@gmail.com>
2018-05-02 19:30:42 +0530

    Update linkgen.php

--- linkgen.php
+++ linkgen.php
@@ -1,37 +1,26 @@
 <?php
-include 'connect.php';
-
 
 $shotmsg = $_POST['shotmsg'];
 
-if (isset($shotmsg)){
-	$r1 = geturl();
-	$stmt = $con->prepare("insert into shots(url1,msg) values(:url1,:msg)");
-	$stmt->bindParam(':url1',$r1);
-	$stmt->bindParam(':msg',$shotmsg);
-	$stmt->execute();
-	$link = "localhost/1shot/".$r1;
-	echo $link;
-	}
+echo link_generator($shotmsg);
 
-
-
-function geturl(){
+function link_generator($shotmsg){
 	include 'connect.php';
-	$r1 = url();
-	$chk = $con->prepare("select from shots where url1=:url1");
-	$chk->bindParam(':url1',$r1);
-	$chk->execute();
-	if($chk->rowCount() > 0){
-		geturl();
+	$url_code = code_gen();
+	$stmt = $con->prepare("insert into shots(url_code,msg) values(:url_code,:msg)");
+	$stmt->bindParam(':url_code',$url_code	);
+	$stmt->bindParam(':msg',$shotmsg);
+	if($stmt->execute()){
+		return "https://oneshot.tk/".$url_code;
 		}
 	else{
-	return $r1;
-	}
+		link_generator();
+		}
 }
 
 
-function url($length = 7) {
+
+function code_gen($length = 7) {
     return substr(str_shuffle(str_repeat($x='0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ',
     ceil($length/strlen($x)) )),1,$length);
 }
