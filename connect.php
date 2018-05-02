c0e2c1c35189c365df5fa28288535fd33340a817
Rishabh Kandari <reevkandari@gmail.com>
2018-05-02 19:33:19 +0530

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,4 +1,10 @@
 
 <?php
 $con = new PDO('mysql:host=localhost;dbname=oneshot;charset:utf8','root','root');
+/*
+
+create table shots(id smallint auto_increment not null primary_key,url_code char(7) not null, msg varchar(255));
+
+*/
+
 ?>
